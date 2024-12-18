0a5cd35cb469b638500a28777b019617b71d1715
teomz <teomz96@outlook.com>
2024-12-19 00:21:12 +0800

    updated page

---
+++ components/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <section>
+        <h2 className='text-3xl font-medium capitalize mb-8'>About me</h2>
+        <p>
+
+        </p>
+        <p>
+
+        </p>
+    </section>
+  )
+}
