e0376408e16711184186297c302d6d18b9f667a3
teomz <teomz96@outlook.com>
2024-12-23 22:27:27 +0800

    Improved Nav Bar Effect

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,10 +8,12 @@ import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
 import Link from 'next/link';
 import { HiDownload } from 'react-icons/hi';
 import { FaGithub } from 'react-icons/fa';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Intro() {
+    const {ref} = useSectionInView('Home', 0.5);
   return (
-    <section className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]' ref={ref} id='home'>
         <div className='flex items-center justify-center'>
             <div className='relative'>
                 <motion.div
